44027e5d98b991f48f1caeac0812c0f5eb1a31aa
amitos2243 <amit.nachum25@gmail.com>
2023-05-15 21:26:18 +0200

    stage 1

--- alum_pro/src/main.ts
+++ alum_pro/src/main.ts
@@ -1,1 +1,13 @@
 import './style.css'
+
+// const contentful = require('contentful')
+import * as contentful from 'contentful'
+const client = contentful.createClient({
+  space: 'ztfmhdgya9hf',
+  environment: 'master', // defaults to 'master' if not set
+  accessToken: 'owzanMo1aPJulLyxQv3wz_3pQU0uy-vuBhLShQAOI60'
+})
+
+client.getEntries()
+.then((response) => console.log(response.items))
+.catch(console.error)

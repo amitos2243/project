2734f7e741b3d6600665cff9f76905a9e2291063
Amit Nachum <121576638+amitos2243@users.noreply.github.com>
2023-06-05 22:41:35 +0200

    Add files via upload

---
+++ project-amit_dev/alum_pro/src/key.ts
@@ -0,0 +1,6 @@
+export const key = 
+{
+    space: 'ztfmhdgya9hf',
+    environment: 'master', // defaults to 'master' if not set
+    accessToken: 'owzanMo1aPJulLyxQv3wz_3pQU0uy-vuBhLShQAOI60'
+  } 

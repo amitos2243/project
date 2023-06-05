99727de06acfdaeccd4990742e954576d32561ee
Amit Nachum <121576638+amitos2243@users.noreply.github.com>
2023-06-05 22:42:23 +0200

    Delete alum_pro directory

--- alum_pro/src/key.ts
+++
@@ -1,6 +0,0 @@
-export const key = 
-{
-    space: 'ztfmhdgya9hf',
-    environment: 'master', // defaults to 'master' if not set
-    accessToken: 'owzanMo1aPJulLyxQv3wz_3pQU0uy-vuBhLShQAOI60'
-  } 
